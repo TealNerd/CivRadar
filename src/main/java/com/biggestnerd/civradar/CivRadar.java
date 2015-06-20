40024e9e4f70f7e8cfc7e1b5a7ab5fe63ede934e
biggestnerd <TealNerd@users.noreply.github.com>
2015-06-19 23:03:45 -0500

    Update CivRadar.java

--- src/main/java/com/biggestnerd/civradar/CivRadar.java
+++ src/main/java/com/biggestnerd/civradar/CivRadar.java
@@ -27,7 +27,7 @@ import com.biggestnerd.civradar.gui.GuiRadarOptions;
 public class CivRadar {
 	public final static String MODID = "civradar";
 	public final static String MODNAME = "CivRadar";
-	public final static String VERSION = "beta-1.2.5";
+	public final static String VERSION = "beta-1.2.8";
 	private RenderHandler renderHandler;
 	private Config radarConfig;
 	private File configFile;
